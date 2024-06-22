2eb1007d88f2843f55cf9d11f45661ec24890851
rythm-sachdeva <rythmrko123@gmail.com>
2024-06-22 19:16:29 +0530

    Firebase Configured

---
+++ src/misc/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBewLyllxlLgb7RVqdnzJ7dFX4gyk-bT5c",
+    authDomain: "chat-web-app-8e139.firebaseapp.com",
+    projectId: "chat-web-app-8e139",
+    storageBucket: "chat-web-app-8e139.appspot.com",
+    messagingSenderId: "1077589412411",
+    appId: "1:1077589412411:web:fbb0fc04b8253fad096a1e"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
